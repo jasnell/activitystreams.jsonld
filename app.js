d1645bb0a605fde61b49f13e5f1258687a49e7fe
James M Snell <jasnell@gmail.com>
2015-04-25 08:32:58 -0700

    update

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
 var port = (process.env.VCAP_APP_PORT || 3000);
 var express = require('express');
-
+var context = require('activitystreams-context');
 var app = express();
-
 app.get('/', function(req, res) {
   res.set({
   	'Access-Control-Allow-Origin': '*',
   	'Content-Location': 'http://activitystrea.ms/2.0/',
     'Content-Type': 'application/ld+json'
   });
-  res.sendfile('activitystreams.jsonld');
+  res.end(JSON.stringify(context));
 });
-
-function setup(id) { 
-  id = id + '.jsonld';
-  app.get('/actions.jsonld', function(req, res) {
-    res.set({
-  	  'Access-Control-Allow-Origin': '*',
-  	  'Content-Location': 'http://activitystrea.ms/2.0/' + id,
-  	  'Content-Type': 'application/ld+json'
-    });
-    res.sendfile(id);
-  });
-}
-
-[ 'actions', 
-  'activitystreams-supplemental', 
-  'schema2as']
-    .forEach(setup);
 
 var server = app.listen(port);
