39912e188d9e77a99a376928b1f92f3d41ddca2a
James M Snell <jasnell@gmail.com>
2015-08-28 14:00:14 -0700

    Update the context, dependencies

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 var port = (process.env.VCAP_APP_PORT || 3000);
 var express = require('express');
 var context = require('activitystreams-context');
+var cors = require('cors');
+var cache = require('cache-control');
 var app = express();
-app.get('/', function(req, res) {
+app.use(cache({
+  '/**': 'public, max-age: 3600'
+}));
+app.get('/', cors(), function(req, res) {
   res.set({
-  	'Access-Control-Allow-Origin': '*',
   	'Content-Location': 'http://www.w3.org/ns/activitystreams',
     'Content-Type': 'application/ld+json'
   });
